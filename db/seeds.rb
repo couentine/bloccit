edd9b2f383c8ac6680276c27ba9104f8a90f9f85
Quentin Guillaume <quent.guillaume@gmail.com>
2017-06-23 23:44:25 +0000

    assignment 17 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,10 @@
    )
  end
  
+ puts"#{Post.count}"
+ Post.find_or_create_by(title:"Title", body:"big body")
+ puts"#{Post.count}"
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
