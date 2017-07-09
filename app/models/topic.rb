f0307fe2c97ff25cde074c7a09dbcf7cc03b4df9
Quentin Guillaume <quent.guillaume@gmail.com>
2017-07-09 18:13:29 +0000

    Correction Variable

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,3 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
-    
-       validates :name, length: { minimum: 5}, presence: true
-   validates :description, length: {minimum: 20}, presence: true
-
-    
 end
