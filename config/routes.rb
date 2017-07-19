23793c2932d17059e57eb894450a2e6b58241596
Quentin Guillaume <quent.guillaume@gmail.com>
2017-07-19 15:54:40 +0000

    bloc 29 done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :posts, only: [] do
+    resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
 
