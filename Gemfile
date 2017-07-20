3da349e659c60e6988f59f5f7c6e80ef2931efd4
Quentin Guillaume <quent.guillaume@gmail.com>
2017-07-20 22:45:29 +0000

    bloc31

--- Gemfile
+++ Gemfile
@@ -33,3 +33,5 @@ end
 gem 'bootstrap-sass'
  # Used for encrypting User passwords
  gem 'bcrypt'
+ 
+  gem 'figaro', '1.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
 
-  it { is_expected. to have_many(:posts) }
-     it { is_expected.to have_many(:comments) }
+   it { is_expected. to have_many(:posts) }
+   it { is_expected.to have_many(:comments) }
+   it { is_expected.to have_many(:votes) }
+   it { is_expected.to have_many(:favorites) }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -51,7 +53,6 @@ RSpec.describe User, type: :model do
        end
      end
  
- # #6
      context "admin user" do
        before do
          user.admin!
@@ -82,4 +83,21 @@ RSpec.describe User, type: :model do
     end
 
   end
+  
+     describe "#favorite_for(post)" do
+     before do
+       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+     end
+ 
+     it "returns `nil` if the user has not favorited the post" do
+       expect(user.favorite_for(@post)).to be_nil
+     end
+ 
+     it "returns the appropriate favorite if it exists" do
+       favorite = user.favorites.where(post: @post).create
+       expect(user.favorite_for(@post)).to eq(favorite)
+     end
+   end
+  
 end
